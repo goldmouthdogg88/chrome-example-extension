32eead3bef24dea24bdda9a52307f28004876630
PTRN444 <ptrn444@iu.edu>
2022-05-12 20:26:56 -0400

    work in progress

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 // background.js
 
-let color = '#3aa757';
+let color = "#3aa757";
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  // prettier-ignore
+  console.log(
+    "Default background color set to %cgreen",
+     `color: ${color}`
+    );
 });
